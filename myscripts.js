3f570ae0a07c38a280dbe40de3a922a1fa6a5f76
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-04-09 15:38:34 -0700

    accessed API and displayed

---
+++ myscripts.js
@@ -0,0 +1,25 @@
+var mainContainer = document.getElementById("myData");
+var typeText = document.getElementById("type")
+var div = document.createElement("div");
+var text = document.getElementById("myData").innerText;
+var button = document.getElementById("button")
+
+//div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+
+function getData() {
+fetch("https://www.boredapi.com/api/activity/")
+    .then(res => {
+        return res.json()
+    })
+    //.then(data =>  console.log(data.activity))
+    .then(data => mainContainer.innerText = data.activity)
+    //.then(data => typeText.innerText = data.type)
+    .catch(error => console.log("error"))
+}
+
+button.addEventListener("click", buttonClick)
+
+function buttonClick() {
+    //document.getElementById("button").innerText = Date();
+    getData()
+  }   
