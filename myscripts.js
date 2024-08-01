c0ff65ffb15bcfed5f6176544b37a7608b86887e
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-08-01 15:18:17 -0700

    updated js as old API has closed down

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,30 @@
 var mainContainer = document.getElementById("myData");
-var typeText = document.getElementById("type")
-var div = document.createElement("div");
-var text = document.getElementById("myData").innerText;
-var button = document.getElementById("button")
+var button = document.getElementById("button");
 
 function getData() {
-fetch("https://www.boredapi.com/api/activity/")
+  const proxyUrl = 'https://api.allorigins.win/get?url=';
+  const targetUrl = encodeURIComponent('https://bored-api.appbrewery.com/random');
+  const uniqueUrl = `${proxyUrl}${targetUrl}&timestamp=${new Date().getTime()}`;
+  
+  fetch(uniqueUrl)
     .then(res => {
-        return res.json()
+      if (!res.ok) {
+        throw new Error('Network response was not ok ' + res.statusText);
+      }
+      return res.json();
     })
-    //.then(data =>  console.log(data.activity))
-    .then(data => mainContainer.innerText = data.activity)
-    .catch(error => console.log("error"))
+    .then(data => {
+      const parsedData = JSON.parse(data.contents);
+      console.log(parsedData);
+      mainContainer.innerText = parsedData.activity;
+    })
+    .catch(error => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
-button.addEventListener("click", buttonClick)
+button.addEventListener("click", buttonClick);
 
 function buttonClick() {
-    getData()
-    
-  }   
+  getData();
+}
